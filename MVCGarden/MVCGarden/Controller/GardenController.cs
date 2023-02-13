9aea36ffe6516991fe1d11a01f197339d5fada0e
Themselfagain <sheikysmar7y@gmail.com>
2023-02-13 11:30:44 +0200

    dobaven display

---
+++ MVCGarden/MVCGarden/Controller/GardenController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVCGarden.Model;
+using MVCGarden.View;
+
+namespace MVCGarden.Controller
+{
+    public class GardenController
+    {
+        private Display display;
+        private Garden garden;
+        public GardenController()
+        {
+            display = new Display();
+            garden = new Garden(display.VegiPrice, display.FruitPrice, display.VegiKg, display.FruitKg);
+            display.Total = garden.CalculatePrice();
+            display.ShowResults();
+
+        }
+    }
+}
---
+++ MVCGarden/MVCGarden/View/Display.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCGarden.View
+{
+    public class Display
+    {
+        public Display()
+        {
+            this.VegiPrice = 0;
+            this.FruitPrice = 0;
+            this.VegiKg = 0;
+            this.FruitKg = 0;
+            this.Total = 0;
+            this.GetValues();
+        }
+
+        public decimal VegiPrice { get; set; }
+        public decimal FruitPrice { get; set; }
+        public int VegiKg { get; set; }
+        public int FruitKg { get; set; }
+        public decimal Total { get; set; }
+        public void GetValues()
+        {
+            this.VegiPrice = decimal.Parse(Console.ReadLine());
+            this.FruitPrice = decimal.Parse(Console.ReadLine());
+            this.VegiKg = int.Parse(Console.ReadLine());
+            this.FruitKg = int.Parse(Console.ReadLine());
+        }
+        public void ShowResults()
+        {
+            Console.WriteLine(this.Total);
+        
+        }
+    }
+}
