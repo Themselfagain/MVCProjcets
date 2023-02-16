8cf5715b4f9467ac9d987446676770d6e7b5f89e
Themselfagain <sheikysmar7y@gmail.com>
2023-02-16 08:56:15 +0200

    MVCTransport Done

---
+++ MVCGarden/CenaTransport/Controller/TransportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CenaTransport.Model;
+using CenaTransport.View;
+
+namespace CenaTransport.Controller
+{
+    public class TransportController
+    {
+        private Display display;
+        private Transport transport;
+        public TransportController()
+        {
+            Display display = new Display();
+            Transport transport = new Transport(display.Km, display.TimeOfDay);
+            display.Total = transport.CalculatedPrice();
+            display.ShowResults();
+        }
+
+
+    }
+}
---
+++ MVCGarden/CenaTransport/Program.cs
@@ -0,0 +1,18 @@
+﻿using CenaTransport.Controller;
+using System;
+
+namespace CenaTransport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransportController controller = new TransportController();
+
+
+
+
+
+        }
+    }
+}
